2355410b37d902b640078fbe7b393e54a5ca026c
hisato <h.takeuchi.engineer@gmail.com>
2024-01-14 18:40:35 +0900

    :package: add:yamada-ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import type { Metadata } from 'next'
 import './globals.css'
+import { colorModeManager, ColorModeScript, UIProvider } from '@yamada-ui/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +13,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ColorModeScript type="cookie" nonce="testing" />
+        <UIProvider colorModeManager={{ ...colorModeManager }.cookieStorage}>{children}</UIProvider>
+      </body>
     </html>
   )
 }
