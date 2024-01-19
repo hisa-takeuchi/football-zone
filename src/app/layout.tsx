2f66ed9975579962d5055a61155ed1ef02c98045
takeuchi <>
2024-01-19 12:07:38 +0900

    :lipstick: feat:skeleton wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
 import { colorModeManager, ColorModeScript, Container, UIProvider } from '@yamada-ui/react'
-import { Inter } from 'next/font/google'
 
 import { Header } from '@/components/layouts/Header/Header'
 
 import type { Metadata } from 'next'
 
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <ColorModeScript type="cookie" nonce="testing" />
         <UIProvider colorModeManager={{ ...colorModeManager }.cookieStorage}>
           <Header />
